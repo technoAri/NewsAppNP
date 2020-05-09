49f716c1ba4f3ee750ce507cc4df666ee2addaac
Arindam <iamari.cse@gmail.com>
2020-05-09 22:32:26 +0530

    initial commit

---
+++ app/src/main/java/com/assignment/newsappnp/ui/main/PageViewModel.java
@@ -0,0 +1,30 @@
+package com.assignment.newsappnp.ui.main;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageViewModel extends ViewModel {
+
+    private List<String> newsList = new ArrayList<String>();
+    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+        @Override
+        public String apply(Integer input) {
+            return "Hello world from section: " + input;
+        }
+    });
+
+    public void setIndex(int index) {
+        mIndex.setValue(index);
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
