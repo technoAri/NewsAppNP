3558148809d16393433c439c42ac7f70646518ef
Arindam <iamari.cse@gmail.com>
2020-05-11 23:09:27 +0530

    UI test cases added

---
+++ app/src/androidTest/java/com/assignment/newsappnp/MainActivityTest.java
@@ -0,0 +1,108 @@
+package com.assignment.newsappnp;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        ViewInteraction frameLayout = onView(
+//                allOf(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class), isDisplayed()));
+//        frameLayout.check(matches(isDisplayed()));
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.iv_thumbnail),
+                        childAtPosition(
+                                allOf(withId(R.id.constraintLayout),
+                                        withParent(withId(R.id.view_pager))),
+                                0),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.tv_author), withText("PTI | "),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.constraintLayout),
+                                        2),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("PTI | ")));
+
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.tv_read_more), withText("  Read More >  "),
+                        childAtPosition(
+                                allOf(withId(R.id.constraintLayout),
+                                        withParent(withId(R.id.view_pager))),
+                                4),
+                        isDisplayed()));
+        textView4.check(matches(withText("  Read More >  ")));
+
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.tv_title), withText("Government likely to make Aarogya Setu app mandatory for flyers post lockdown - Times of India"),
+                        childAtPosition(
+                                allOf(withId(R.id.constraintLayout),
+                                        withParent(withId(R.id.view_pager))),
+                                1),
+                        isDisplayed()));
+        textView6.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
